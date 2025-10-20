69ea849ef340b6b5bbd0a6271dc8d5ed97aff17b
null <greatnull2@gmail.com>
2025-10-20 16:02:30 +0900

    fix: .gitignore 적용

---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+import Navigation from "../components/navigation";
+
+export default function NotFount() {
+
+    return (
+        <div>
+            <Navigation />
+            <h1>NOT FOUND !</h1>
+        </div>
+
+    )
+
+}
